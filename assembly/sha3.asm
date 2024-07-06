8b8c7a5a43154534b5987398eb2a54c7c6fdb748
Cormac <71203678+CormacZ@users.noreply.github.com>
2024-07-06 09:54:43 +0800

    Update sha3.asm

--- assembly/sha3.asm
+++ assembly/sha3.asm
@@ -1,19 +1,82 @@
-; sha3.asm - Example implementation of SHA3-512 hashing in assembly
+; assembly/sha3.asm
 
-section .data
-    ; Constants for SHA3-512
-    RC: times 24 dq 0
-    KeccakRhoOffsets: db 0, 36, 3, 41, 18, 1, 44, 10, 45, 2, 62, 6, 43, 15, 61, 28, 55, 25, 21, 56, 27, 20, 39, 8
-    KeccakRoundConstants: dq 0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000
-                         dq 0x000000000000808b, 0x8000000000008003, 0x8000000000008002, 0x8000000000000080
-                         dq 0x000000000000800a, 0x800000008000000a, 0x8000000080008081, 0x8000000000008080
-                         dq 0x0000000080000001, 0x8000000080008008, 0x8000000000008009, 0x800000000000000a
-                         dq 0x000000000000008a, 0x0000000000000088, 0x0000000080008009, 0x000000008000000a
-                         dq 0x000000008000808b, 0x800000000000008b, 0x8000000000008089, 0x8000000000008003
-                         dq 0x8000000000008002, 0x8000000000000080, 0x000000000000800a, 0x800000008000000a
-                         dq 0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
-                         dq 0x8000000000008009, 0x800000000000000a, 0x000000000000008a, 0x0000000000000088
-                         dq 0x0000000080008009, 0x0000000080000008, 0x800000000000808b, 0x800000000000008b
-                         dq 0x8000000000008089, 0x8000000000008003, 0x8000000000008002, 0x0000000000000080
-                         dq 0x800000000000800a, 0x000000008000000a, 0x8000000080008081, 0x8000000000008080
-                         dq 0x0000000080000001, 0x8000000080008008, 0x8000000000008009, 0x800000000000000
+section .text
+global sha3_512
+
+; Constants for SHA3-512
+%define ROUNDS 24
+%define STATE_SIZE 200
+
+; Function to perform SHA3-512 hashing
+sha3_512:
+    ; Arguments:
+    ; rdi: input message (pointer to input)
+    ; rsi: input length (length of input in bytes)
+    ; rdx: output buffer (pointer to output)
+
+    ; Save registers that will be used
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+
+    ; Initialize SHA3-512 state
+    ; State is 200 bytes (1600 bits)
+    ; Clear the state buffer
+    xor r12, r12
+    mov r14, rdx         ; r14 = output buffer
+
+    ; Absorb phase
+    mov rsi, rsi         ; rsi = input length
+    xor rdx, rdx         ; rdx = 0 (offset)
+    xor rcx, rcx         ; rcx = 0 (index)
+    xor rbx, rbx         ; rbx = 0 (temporary)
+
+    absorb_loop:
+        cmp rcx, rsi
+        jge squeeze_phase
+
+        ; XOR input with state
+        movzx r15, byte [rdi + rcx]
+        mov [r12 + rdx], r15
+        inc rcx
+
+        ; Absorb
+        cmp rdx, STATE_SIZE
+        jge keccak_f
+
+        jmp absorb_loop
+
+    ; Keccak-f permutation
+    keccak_f:
+        ; Keccak-f function (omitted for brevity)
+        ; Requires implementation of permutation function
+
+    ; Squeeze phase
+    squeeze_phase:
+        ; Squeeze state into output buffer
+        mov rcx, 0          ; rcx = 0 (index)
+        xor rdx, rdx        ; rdx = 0 (offset)
+
+    squeeze_loop:
+        cmp rcx, STATE_SIZE
+        jge done
+
+        ; Copy state to output buffer
+        mov r15, [r12 + rdx]
+        mov [r14 + rcx], r15
+        inc rcx
+
+        jmp squeeze_loop
+
+    done:
+        ; Restore registers
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbx
+
+    ; Return to caller
+    ret
